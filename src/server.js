cca00bbd8264b8ab1342179604392fc650fab9c1
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-07-07 21:25:51 +0200

    update

--- src/server.js
+++ src/server.js
@@ -33,16 +33,16 @@ app.use((req, res, next) => {
     return res.status(404).json({ message: error.message })
 })
 
-const credentials = {
-    key: fs.readFileSync(path.join(__dirname, process.env.CERT_KEY)),
-    cert: fs.readFileSync(path.join(__dirname, process.env.CERT))
-}
-const HTTPS_PORT = process.env.HTTPS_PORT ?? 6443
+// const credentials = {
+//     key: fs.readFileSync(path.join(__dirname, process.env.CERT_KEY)),
+//     cert: fs.readFileSync(path.join(__dirname, process.env.CERT))
+// }
+// const HTTPS_PORT = process.env.HTTPS_PORT ?? 6443
 const HTTP_PORT = process.env.HTTP_PORT ?? 6080
-https.createServer(credentials, app)
-.listen(HTTPS_PORT, () => {
-    console.log('The server is running on https://localhost:' + HTTPS_PORT + "/api")
-})
+// https.createServer(credentials, app)
+// .listen(HTTPS_PORT, () => {
+//     console.log('The server is running on https://localhost:' + HTTPS_PORT + "/api")
+// })
 
 app.listen(HTTP_PORT, () => {
     console.log('The server is running on http://localhost:' + HTTP_PORT + "/api")
